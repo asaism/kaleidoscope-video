967e3e42a7960bdc15a05799cc91ffcd70709b5c
Suguru Asai <asai@uni-kk.net>
2017-07-13 09:09:45 +0900

    put out javascript

---
+++ js/kaleidoscope.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    startVideo();
+};
+
+function startVideo() {
+    let localVideos = document.getElementsByTagName('video');
+    let localStream;
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(stream) {
+        localStream = stream;
+        [].forEach.call(localVideos, function(video) {
+            setTimeout(function() {
+                video.src =  window.URL.createObjectURL(localStream);
+            }, 0);
+        });
+    }).catch(function(error) {
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
+}
